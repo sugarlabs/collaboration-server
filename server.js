f3b620f9cfb8a8a1e05c5bc2c58c61a098c65bbb
apsinghdev <ajeetpratap517@gmail.com>
2024-06-14 14:14:24 +0530

    Implement cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import Express from 'express';
+import cors from 'cors';
 
 const App = Express();
 const PORT = 5000;
+
+App.use(cors({origin: '*'}));
 
 App.use('/hi', (req, res) => {
     res.send({hi: 'hi'})
