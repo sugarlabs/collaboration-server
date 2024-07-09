52662b7477d381a33f06d09b998dbfb375e77ba8
apsinghdev <ajeetpratap517@gmail.com>
2024-07-09 16:39:29 +0530

    Major Fixes

--- server.js
+++ server.js
@@ -1,56 +1,42 @@
-import Express from "express";
+import express from "express";
 import cors from "cors";
 import * as http from "http";
 import { Server } from "socket.io";
-import { YSocketIO } from "y-socket.io/dist/server";
-import * as Y from "yjs";
 
-const app = Express();
-const PORT = 5000;
+const app = express();
+const PORT = 8080;
 
-// Map to store yjs docs by room name
-const docs = new Map();
-
+// Enable CORS
 app.use(cors({ origin: "*" }));
 
+// Create HTTP server
 const server = http.createServer(app);
 
-const io = new Server(server);
-const ysocketio = new YSocketIO(io, {});
-ysocketio.initialize();
+// Create Socket.IO server
+const io = new Server(server, {cors: {origin: "*", methods: ["GET", "POST"]}});
 
+// Socket.IO connection event
 io.on("connection", (socket) => {
   console.log(`[connection] connected with user: ${socket.id}`);
 
-  socket.on('joinRoom', (room) => {
-    if(!docs.has(room)){
-      const ydoc = new Y.Doc();
-      docs.set(room, ydoc);
-    }
-    socket.join(room);
-    console.log(`User joined room: ${room}`);
+//  io.to(socket.id).emit(console.log("connected in room"));
+
+  socket.on("message", (msg) => {
+    console.log('message: ' + msg);
+    socket.emit('message', 'Server received: ' + msg);
   });
 
-  socket.on("message", (room, message) => {
-    io.to(room).emit(message);
-  })
-
-  socket.on("leaveRoom", (room) => {
-    socket.leave(room);
-    console.log(`User left the room: ${room}`);
-  })
-
   socket.on("disconnect", () => {
-    console.log(`[disconnected] disconnected with user: ${socket.id}`);
+    console.log(`user disconnected: ${socket.id}`);
   });
 });
 
-
-
+// Simple test endpoint
 app.get("/test", (req, res) => {
   res.send(JSON.stringify({ ok: true }));
 });
 
-app.listen(PORT, () => {
+// Listen on the defined PORT
+server.listen(PORT, () => {
   console.log(`Server's up and running on port ${PORT} `);
 });
