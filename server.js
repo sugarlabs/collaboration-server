5d150c9c31e53ac846892260d71f5520ed6896da
apsinghdev <ajeetpratap517@gmail.com>
2024-07-29 20:42:33 +0530

    implement collab

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import * as http from "http";
 import { Server } from "socket.io";
+import * as Y from "yjs";
 
 const app = express();
 const PORT = 8080;
 
 // Enable CORS
 app.use(cors({ origin: "*" }));
+
+// Create a central Yjs doc
+// const doc = new Y.Doc();
 
 // Create HTTP server
 const server = http.createServer(app);
@@ -21,10 +25,39 @@ io.on("connection", (socket) => {
 
 //  io.to(socket.id).emit(console.log("connected in room"));
 
+  socket.on("joinRoom", (room_id) => {
+    socket.join(room_id);
+    console.log(`User ${socket.id} joined room ${room_id}`);
+  })
+
   socket.on("message", (msg) => {
     console.log('message: ' + msg);
     socket.emit('message', 'Server received: ' + msg);
   });
+
+  socket.on("new-block-added", (data) => {
+    const {room_id, update} = data;
+    socket.to(room_id).emit("new-block-added", update);
+    console.log(`user ${socket.id} sent the changes in room ${room_id}`);
+  })
+
+  socket.on("new-block-deleted", (data) => {
+  const {room_id, update} = data;
+  socket.to(room_id).emit("new-block-deleted", update);
+  console.log(`user ${socket.id} deleted the block in room ${room_id}`);
+  })
+
+  socket.on("block-moved/connected/disconnected", (data) => {
+  const {room_id, update} = data;
+  socket.to(room_id).emit("block-moved/connected/disconnected", update);
+  console.log(`user ${socket.id} changed the block in room ${room_id}`);
+  })
+
+  socket.on("block-value-updated", (data) => {
+  const {room_id, update} = data;
+  socket.to(room_id).emit("block-value-updated", update);
+  console.log(`user ${socket.id} changed the value of a block in room ${room_id}`);
+  })
 
   socket.on("disconnect", () => {
     console.log(`user disconnected: ${socket.id}`);
