26683b312f650af0aa75cf053e78ea809b85bf9f
apsinghdev <ajeetpratap517@gmail.com>
2024-08-28 15:53:33 +0530

    cleanup

--- server2.js
+++ server2.js
@@ -7,52 +7,38 @@ import * as Y from "yjs";
 const app = express();
 const PORT = 8080;
 
-// Enable CORS
 app.use(cors({ origin: "*" }));
 
-// Create a central Yjs doc
-// const doc = new Y.Doc();
-
-// Create HTTP server
 const server = http.createServer(app);
-
-// Create Socket.IO server
-const io = new Server(server, {cors: {origin: "*", methods: ["GET", "POST"]}});
+const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
 const rooms = new Map();
 
-// Socket.IO connection event
 io.on("connection", (socket) => {
   console.log(`[connection] connected with user: ${socket.id}`);
 
-//  io.to(socket.id).emit(console.log("connected in room"));
-
-  socket.on("joinRoom", ({room_id, name}) => {
+  socket.on("joinRoom", ({ room_id, name }) => {
     socket.join(room_id);
 
-    if(!rooms.has(room_id)){
-	rooms.set(room_id, new Set);
-  };
+    if (!rooms.has(room_id)) {
+      rooms.set(room_id, new Map);
+    };
 
     const room = rooms.get(room_id);
-    room.add(socket.id);
+    room.set(socket.id, name);
 
-//    if (room.size > 1) {
+    socket.emit("add-existing-names", Array.from(room).map(([id, name]) => ({ id, name })));
 
-//    socket.emit("add-existing-names", Array.from(room).map((id, name) => ({id, name})));
+    socket.to(room_id).emit("add-new-name", { id: socket.id, name });
 
-//    socket.to(room_id).emit("add-new-name", {id: socket.id, name});
+    socket.emit("existing-cursor", Array.from(room).map(id => ({ id })));
 
-    socket.emit("existing-cursor", Array.from(room).map(id => ({id})));
-
-    socket.to(room_id).emit("new-cursor", {id: socket.id});
-
-// }
+    socket.to(room_id).emit("new-cursor", { id: socket.id });
 
     console.log(`User ${socket.id} joined room ${room_id}`);
   })
   socket.on("mouse-move", (data) => {
     const { room_id, x, y } = data;
-    socket.to(room_id).emit("mouse-move", {socket_id: socket.id, x, y});
+    socket.to(room_id).emit("mouse-move", { socket_id: socket.id, x, y });
   })
 
   socket.on("message", (msg) => {
@@ -61,40 +47,40 @@ io.on("connection", (socket) => {
   });
 
   socket.on("new-block-added", (data) => {
-    const {room_id, update} = data;
+    const { room_id, update } = data;
     socket.to(room_id).emit("new-block-added", update);
     console.log(`user ${socket.id} sent the changes in room ${room_id}`);
   })
 
   socket.on("new-block-deleted", (data) => {
-  const {room_id, update} = data;
-  socket.to(room_id).emit("new-block-deleted", update);
-  console.log(`user ${socket.id} deleted the block in room ${room_id}`);
+    const { room_id, update } = data;
+    socket.to(room_id).emit("new-block-deleted", update);
+    console.log(`user ${socket.id} deleted the block in room ${room_id}`);
   })
 
   socket.on("block-moved/connected/disconnected", (data) => {
-  const {room_id, update} = data;
-  socket.to(room_id).emit("block-moved/connected/disconnected", update);
-  console.log(`user ${socket.id} changed the block in room ${room_id}`);
+    const { room_id, update } = data;
+    socket.to(room_id).emit("block-moved/connected/disconnected", update);
+    console.log(`user ${socket.id} changed the block in room ${room_id}`);
   })
 
   socket.on("block-value-updated", (data) => {
-  const {room_id, update} = data;
-  socket.to(room_id).emit("block-value-updated", update);
-  console.log(`user ${socket.id} changed the value of a block in room ${room_id}`);
+    const { room_id, update } = data;
+    socket.to(room_id).emit("block-value-updated", update);
+    console.log(`user ${socket.id} changed the value of a block in room ${room_id}`);
   })
 
   socket.on("disconnect", () => {
     rooms.forEach((room, room_id) => {
-      if(room.has(socket.id)){
-      room.delete(socket.id);
-      io.to(room_id).emit("remove-cursor", socket.id)
-	if(room.size === 0){
-	rooms.delete(room_id);
-        console.log(`Room ${room_id} has been removed`);
-    }
-    }
-   })
+      if (room.has(socket.id)) {
+        room.delete(socket.id);
+        io.to(room_id).emit("remove-cursor", socket.id)
+        if (room.size === 0) {
+          rooms.delete(room_id);
+          console.log(`Room ${room_id} has been removed`);
+        }
+      }
+    })
     console.log(`user disconnected: ${socket.id}`);
   });
 });
@@ -104,7 +90,6 @@ app.get("/test", (req, res) => {
   res.send(JSON.stringify({ ok: true }));
 });
 
-// Listen on the defined PORT
 server.listen(PORT, () => {
   console.log(`Server's up and running on port ${PORT} `);
 });
