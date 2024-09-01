56e99f5e62f86a761d494faa09a6e74ea4534830
apsinghdev <ajeetpratap517@gmail.com>
2024-09-01 14:43:45 +0530

    install and configure dotenv

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import * as http from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 8080;
